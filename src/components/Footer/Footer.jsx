debdbb69726e06e11bb7a42a1e29b7f7ec62bd10
awo-ali <awoali@hotmail.co.uk>
2022-08-31 14:51:11 +0100

    added links to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import "./Footer.scss";
-import github from "../../assets/images/github-icon.png"
-import linkedin from "../../assets/images/linkedin-icon.png"
-import CV from "../../assets/images/cv-icon.png"
+import github from "../../assets/images/github-icon.png";
+import linkedin from "../../assets/images/linkedin-icon.png";
+import CV from "../../assets/images/cv-icon.png";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <div id="contact" className="footer">
       <div className="footer__email">awoali@hotmail.co.uk</div>
-      <div className="footer__info">
-        <div className="footer__github">
+
+      <ul className="footer__info">
+        <li>
+          <a href="https://github.com/awo-ali/portfolio"  className="footer__github">
+            <img className="footer__icons" src={github} alt="" />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.linkedin.com/in/awo-ali/" className="footer__linkedin">
+            <img className="footer__icons" src={linkedin} alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="file:///C:/Users/Student/Documents/nology/Awo_Ali_Profile.pdf"  className="footer__cv">
+            <img className="footer__icon-cv" src={CV} alt="" />
+          </a>
+        </li>
+      </ul>
+      {/* <div className="footer__info">         
+      <div className="footer__github">
           <img className="footer__icons" src={github} alt="" />
         </div>
         <div className="footer__linkedin">
@@ -18,7 +37,7 @@ const Footer = () => {
         <div className="footer__cv">
         <img className="footer__icon-cv"  src={CV} alt="" />
         </div>
-      </div>
+       </div> */}
     </div>
   );
 };
