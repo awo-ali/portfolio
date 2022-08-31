657e4d98f433228f62b2be6ff9a36f8f5e4489bc
awo-ali <awoali@hotmail.co.uk>
2022-08-31 14:54:39 +0100

    importing components to main

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -4,13 +4,17 @@ import Home from "../../components/Home/Home";
 import Skills from "../../components/Skills/Skills";
 import Footer from "../../components/Footer/Footer";
 import About from "../../components/About/About"
+// import Project from "../../components/Project/Project";
+import ProjectList from "../../components/ProjectList/ProjectList";
 
 
-const Main = () => {
+const Main = ({projects}) => {
   return (
     <div className="main">
       <Home/>
       <Skills/>
+      {/* <Project/> */}
+      <ProjectList projects={projects}/>
       <About/>
       <Footer/>
 
