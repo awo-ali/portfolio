4de0393a4112a40b58c7fe292a2bbd79afd9f56b
awo-ali <awoali@hotmail.co.uk>
2022-08-31 12:38:16 +0100

    created data file

---
+++ src/projects.js
@@ -0,0 +1,39 @@
+export const projectsArray =
+
+[
+    {
+        "language":"REACT",
+        "title":"Client Project",
+        "description":"Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
+
+    },
+    {
+        "language":"JAVE, SPRING BOOT",
+        "title":"REST API",
+        "description":"Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
+    },
+    {
+        "language":"REACT",
+        "title":"Punk API",
+        "description":"Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
+
+    },
+    {
+        "language":"REACT",
+        "title":"REST API",
+        "description":"Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
+    },
+    {
+        "language":"REACT",
+        "title":"Punk API",
+        "description":"Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
+    }, 
+    {
+        "language":"REACT",
+        "title":"Client Project",
+        "description":"Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
+    }
+
+];
+
+export default projectsArray;
