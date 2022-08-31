09c42575f92ad7370febfe8c90fefbf6d3c285ce
awo-ali <awoali@hotmail.co.uk>
2022-08-31 14:54:00 +0100

    addng links to navbar

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -5,12 +5,32 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar__logo">Awo Ali</div>
-      <div className="navbar__links">
+      <ul className="navbar__links">
+        <li>
+          <a className="navbar__links-styling" href="#home">
+            Home
+          </a>
+        </li>
+        <li>
+          <a className="navbar__links-styling" href="#about">
+            About
+          </a>
+        </li>
+        <li>
+          <a className="navbar__links-styling" href="#projects">
+            Projects
+          </a>
+        </li>
+        <a href="#contact">
+          <button className="navbar__contact">Get in Touch</button>
+        </a>
+      </ul>
+      {/* <div className="navbar__links">
         <div className="navbar__links-styling">Home</div>
         <div className="navbar__links-styling">About</div>
         <div className="navbar__links-styling">Projects</div>
         <button className="navbar__contact">Get in Touch</button>
-      </div>
+      </div> */}
     </div>
   );
 };
