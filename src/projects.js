30f9586897aa753ba28cb6f8a39add68fc9d6e16
awo-ali <awoali@hotmail.co.uk>
2022-09-13 17:48:33 +0100

    changes to data

--- src/projects.js
+++ src/projects.js
@@ -1,39 +1,38 @@
-export const projectsArray =
+import player from "./assets/images/playerCards.png";
+import beer from "./assets/images/beer-cards.png";
+import ticket from "./assets/images/ticket-tracker.png";
 
-[
-    {
-        "language":"REACT",
-        "title":"Client Project",
-        "description":"Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
-
-    },
-    {
-        "language":"JAVA, SPRING BOOT",
-        "title":"Full-stack project",
-        "description":"Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
-    },
-    {
-        "language":"REACT",
-        "title":"Punk API",
-        "description":"An app built using Brewdogs Punk API. A catalogue of beers is generated that can then be filtered and searched through.",
-
-    },
-    {
-        "language":"REACT",
-        "title":"Ticket Tracker",
-        "description":"Ticket tracker app .",
-    },
-    {
-        "language":"VANILLA JAVASCRIPT",
-        "title":"Calculator",
-        "description":"Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
-    }, 
-    {
-        "language":"VANILLA JAVASCRIPT",
-        "title":"Quiz",
-        "description":"Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
-    }
-
+export const projectsArray = [
+  {
+    id: "1",
+    image: player,
+    language: "JAVA, SPRING BOOT",
+    title: "Full-stack project",
+    description:
+      "Maecenas accumsan tincidunt id et in ut diam sit. Metus, nec, augue urna gravida nisl, tortor sapien nec sollicitudin.",
+    code: "https://github.com/awo-ali/full-stack-project",
+    preview:"https://awo-ali.github.io/beer-cards-challenge/"
+  },
+  {
+    id: "2",
+    image: beer,
+    language: "REACT",
+    title: "Punk API",
+    description:
+      "An app built using Brewdogs Punk API. A catalogue of beers is generated that can then be filtered and searched through.",
+    code: "https://github.com/awo-ali/beer-cards-challenge",
+    preview:"https://awo-ali.github.io/beer-cards-challenge/"
+  },
+  {
+    id: "3",
+    image: ticket,
+    language: "REACT",
+    title: "Ticket Tracker",
+    description:
+      "Ticket tracker app, built in react, with an add an employee function.",
+    code: "https://github.com/awo-ali/ticket-tracker-challenge",
+    preview:"https://awo-ali.github.io/beer-cards-challenge/"
+  },
 ];
 
 export default projectsArray;
